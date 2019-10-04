755a59de973f6de0bc6ee891336d54082a2f195f
Veli Veli <VeliAhmed.Veli@mhr.co.uk>
2019-10-04 17:31:46 +0100

    feat(core): allow stopping the line

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
 const PORT = Number(process.env.PORT) || 8080;
+import * as bodyParser from 'body-parser';
 import * as express from 'express';
 
 const app = express();
 
-app.get('/stoptheline', (req, res) => {
-  console.log('stopped the line');
-  res.send('line stopped');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post('/stoptheline', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  res.end(`User ${req.body.user_name} stopped the line`);
 });
 
 app.listen(PORT, () => {
